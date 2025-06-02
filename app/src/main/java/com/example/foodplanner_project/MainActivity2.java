4611608a6aa8b3955d1c712469cd3ea389d6c047
Tasneem Tarek <tasneemtarek951@gmail.com>
2025-06-02 22:33:18 +0300

    enhancment

--- app/src/main/java/com/example/foodplanner_project/MainActivity2.java
+++ app/src/main/java/com/example/foodplanner_project/MainActivity2.java
@@ -34,8 +34,8 @@ import db.Repository;
 
 public class MainActivity2 extends AppCompatActivity implements LogoutView {
 
-    NavController navController;
     BottomNavigationView bottomNavigationView;
+    NavController navController;
     private LogoutPresenter presenter;
     String username;
     public static String type;
@@ -47,15 +47,12 @@ public class MainActivity2 extends AppCompatActivity implements LogoutView {
         setContentView(R.layout.activity_main2);
 
         presenter = new LogoutpresenterImp(this, this, this);
-
         isConnected = NetworkUtils.isConnected(this);
 
-        // Setup Navigation
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-        // Retrieve user data
         Intent intent = getIntent();
         username = intent.getStringExtra(MainActivity.username);
         type = intent.getStringExtra(MainActivity.type);
@@ -64,14 +61,34 @@ public class MainActivity2 extends AppCompatActivity implements LogoutView {
             Toast.makeText(this, "No internet connection. Some features are disabled.", Toast.LENGTH_SHORT).show();
         }
 
-        if (type != null && type.equals("Guest")) {
-            disableGuestFeatures();
-        }
+        // Disable features based on guest or connection
+        updateMenuItemsBasedOnUserTypeAndConnection();
+
+        // Manual handling for logout
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.logout) {
+                presenter.logout();
+                return true;
+            } else {
+                NavigationUI.onNavDestinationSelected(item, navController);
+                return true;
+            }
+        });
     }
 
-    private void disableGuestFeatures() {
-        bottomNavigationView.getMenu().findItem(R.id.favoriteFragment).setEnabled(false);
-        bottomNavigationView.getMenu().findItem(R.id.myPlaneFragment).setEnabled(false);
+    private void updateMenuItemsBasedOnUserTypeAndConnection() {
+        Menu menu = bottomNavigationView.getMenu();
+
+        if (!isConnected) {
+            menu.findItem(R.id.homeFragment).setEnabled(false);
+            menu.findItem(R.id.searchFragment).setEnabled(false);
+            menu.findItem(R.id.logout).setEnabled(false);
+        }
+
+        if ("Guest".equals(type)) {
+            menu.findItem(R.id.favoriteFragment).setEnabled(false);
+            menu.findItem(R.id.myPlaneFragment).setEnabled(false);
+        }
     }
 
     @Override
